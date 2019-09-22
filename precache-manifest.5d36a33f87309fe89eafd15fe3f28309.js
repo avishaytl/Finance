e9f526277a686c246be7a96283c0acb40911f9de
avishay <avishaytl@gmail.com>
2019-09-22 14:27:46 +0300

    Updates

---
+++ precache-manifest.5d36a33f87309fe89eafd15fe3f28309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33665bcc0e63d996d221d5560e2d1f2e",
+    "url": "/Finance/index.html"
+  },
+  {
+    "revision": "dc7bb10e8124cedf8c56",
+    "url": "/Finance/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "35cb2855e8dc30d144b3",
+    "url": "/Finance/static/js/2.95895f1e.chunk.js"
+  },
+  {
+    "revision": "dc7bb10e8124cedf8c56",
+    "url": "/Finance/static/js/main.7d7006d7.chunk.js"
+  },
+  {
+    "revision": "97d69b21400b8d62975d",
+    "url": "/Finance/static/js/runtime-main.fd30ced6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Finance/static/media/logo.25bf045c.svg"
+  }
+]);
